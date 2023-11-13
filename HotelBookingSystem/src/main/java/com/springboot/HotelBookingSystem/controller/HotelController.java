e598f3f89702fb5e56736f8c4afe9458a675b2af
unknown <rejetipratyusha001@gmail.com>
2023-11-13 21:20:11 +0530

    Hotel detail flow

---
+++ HotelBookingSystem/src/main/java/com/springboot/HotelBookingSystem/controller/HotelController.java
@@ -0,0 +1,145 @@
+package com.springboot.HotelBookingSystem.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.springboot.HotelBookingSystem.exception.InvalidIdException;
+import com.springboot.HotelBookingSystem.model.Executive;
+import com.springboot.HotelBookingSystem.model.Hotel;
+import com.springboot.HotelBookingSystem.model.HotelAdmin;
+import com.springboot.HotelBookingSystem.model.Location;
+import com.springboot.HotelBookingSystem.service.ExecutiveService;
+import com.springboot.HotelBookingSystem.service.HotelAdminService;
+import com.springboot.HotelBookingSystem.service.HotelService;
+import com.springboot.HotelBookingSystem.service.LocationService;
+
+@RestController
+@RequestMapping("/hotel")
+public class HotelController {
+
+	@Autowired
+	private ExecutiveService executiveService;
+
+	@Autowired
+	private HotelService hotelService;
+
+	@Autowired
+	private HotelAdminService hotelAdminService;
+
+	@Autowired
+	private LocationService locationService;
+
+	@PostMapping("/add/{aid}/{lid}")
+	public ResponseEntity<?> postHotel(@PathVariable("aid") int aid, @PathVariable("lid") int lid,
+			@RequestBody Hotel hotel) {
+
+		try {
+			// Getting the Admin details from admin id
+			HotelAdmin adminDetails = hotelAdminService.getAdminById(aid);
+
+			// Getting the executive details from Admin
+			Executive executive = adminDetails.getExecutive();
+
+			// Getting the location details by location Id
+			Location location = locationService.getLocationById(lid);
+
+			// Setting adminDetails to hotel
+			hotel.setHotelAdmin(adminDetails);
+
+			// Setting executive details to hotel
+			hotel.setExecutive(executive);
+
+			// Setting location to hotel
+			hotel.setLocation(location);
+
+			hotel = hotelService.postHotel(hotel);
+			return ResponseEntity.ok().body(hotel);
+		} catch (InvalidIdException e) {
+			return ResponseEntity.badRequest().body(e.getMessage());
+		}
+
+	}
+
+	@GetMapping("/getallhotels")
+	public List<Hotel> getAllHotels(@RequestParam(value = "page", required = false, defaultValue = "0") Integer page,
+			@RequestParam(value = "size", required = false, defaultValue = "1000000") Integer size) {
+		Pageable pageable = PageRequest.of(page, size);
+		return hotelService.getAllHotels(pageable);
+
+	}
+
+	@GetMapping("/getAllByExecutive/{eid}")
+	public ResponseEntity<?> getAllHotelsByEid(@PathVariable("eid") int eid) {
+		try {
+			// fetch executive obj using executive id
+			Executive executive = executiveService.getOne(eid);
+
+			// get all hotels using executive id
+			List<Hotel> list = hotelService.getAllHotelsByEid(eid);
+
+			return ResponseEntity.ok().body(list);
+		} catch (InvalidIdException e) {
+			return ResponseEntity.badRequest().body(e.getMessage());
+
+		}
+	}
+
+	@GetMapping("/getAllByLocation/{lid}")
+	public ResponseEntity<?> getAllHotelsByLid(@PathVariable("lid") int lid) {
+		try {
+			// fetch location obj using location id
+			Location location = locationService.getOne(lid);
+
+			// get all hotels using location id
+			List<Hotel> list = hotelService.getAllHotelsByLocation(lid);
+
+			return ResponseEntity.ok().body(list);
+		} catch (InvalidIdException e) {
+			return ResponseEntity.badRequest().body(e.getMessage());
+
+		}
+	}
+
+	@GetMapping("/getByHotel/{hid}")
+	public ResponseEntity<?> getHotelsByHid(@PathVariable("hid") int hid) {
+		try {
+			// get all hotels using hotel id
+			Hotel hotel = hotelService.getHotelsByHid(hid);
+
+			return ResponseEntity.ok().body(hotel);
+		} catch (InvalidIdException e) {
+			return ResponseEntity.badRequest().body(e.getMessage());
+
+		}
+	}
+
+	@DeleteMapping("/delete/{id}")
+	public ResponseEntity<?> deleteHotel(@PathVariable("id") int id) {
+
+		try {
+
+			// validate id
+			Hotel hotel = hotelService.getOne(id);
+
+			// delete
+			hotelService.deleteHotel(hotel);
+			return ResponseEntity.ok().body("hotel deleted successfully");
+
+		} catch (InvalidIdException e) {
+			return ResponseEntity.badRequest().body(e.getMessage());
+		}
+	}
+
+}
---
+++ HotelBookingSystem/src/main/java/com/springboot/HotelBookingSystem/service/HotelService.java
@@ -0,0 +1,56 @@
+package com.springboot.HotelBookingSystem.service;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+
+import com.springboot.HotelBookingSystem.exception.InvalidIdException;
+import com.springboot.HotelBookingSystem.model.Hotel;
+import com.springboot.HotelBookingSystem.repository.HotelRepository;
+
+@Service
+public class HotelService {
+
+	@Autowired
+	private HotelRepository hotelRepository;
+
+	public Hotel postHotel(Hotel hotel) {
+		return hotelRepository.save(hotel);
+	}
+
+	public List<Hotel> getAllHotels(Pageable pageable) {
+		return hotelRepository.findAll(pageable).getContent();
+	}
+
+	public List<Hotel> getAllHotelsByEid(int eid) {
+		return hotelRepository.findByExecutiveId(eid);
+	}
+
+	public List<Hotel> getAllHotelsByLocation(int lid) {
+		return hotelRepository.findByLocationId(lid);
+	}
+
+	public Hotel getHotelsByHid(int hid) throws InvalidIdException {
+		Optional<Hotel> optional = hotelRepository.findById(hid);
+		if (!optional.isPresent()) {
+			throw new InvalidIdException("location id invalid");
+		}
+		return optional.get();
+	}
+
+	public Hotel getOne(int id) throws InvalidIdException {
+		Optional<Hotel> optional = hotelRepository.findById(id);
+		if (!optional.isPresent()) {
+			throw new InvalidIdException("hotel id invalid");
+		}
+		return optional.get();
+	}
+
+	public void deleteHotel(Hotel hotel) {
+		hotelRepository.delete(hotel);
+	}
+
+}
