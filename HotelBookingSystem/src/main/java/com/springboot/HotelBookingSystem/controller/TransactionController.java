4540fd7e45726f937292828cac7960459aae09dc
Sai Mohith <saimohith.2017@gmail.com>
2023-11-13 20:32:16 +0530

    transaction updated

--- HotelBookingSystem/src/main/java/com/springboot/HotelBookingSystem/controller/TransactionController.java
+++ HotelBookingSystem/src/main/java/com/springboot/HotelBookingSystem/controller/TransactionController.java
@@ -1,9 +1,15 @@
 package com.springboot.HotelBookingSystem.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.springboot.HotelBookingSystem.model.Transaction;
@@ -22,4 +28,14 @@ public class TransactionController {
 		return transactionService.insert(transaction);
 	}
 	
+	@GetMapping("/transaction/getall")
+	public List<Transaction> getAll(@RequestParam(value = "page", required = false, defaultValue = "0") Integer page,
+			@RequestParam(value = "size", required = false, defaultValue = "1000000") Integer size)
+	{
+		Pageable pageable = PageRequest.of(page,size);
+		return transactionService.getAll(pageable);
+		
+		
+		
+		}
 }
