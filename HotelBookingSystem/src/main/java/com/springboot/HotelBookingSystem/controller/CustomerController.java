3607b05fef636b24a98e670a475317dd6d6d9206
Sai Mohith <saimohith.2017@gmail.com>
2023-11-13 20:46:48 +0530

    customer getone added

--- HotelBookingSystem/src/main/java/com/springboot/HotelBookingSystem/controller/CustomerController.java
+++ HotelBookingSystem/src/main/java/com/springboot/HotelBookingSystem/controller/CustomerController.java
@@ -5,8 +5,10 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -14,6 +16,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.springboot.HotelBookingSystem.enumerate.Role;
+import com.springboot.HotelBookingSystem.exception.InvalidIdException;
 import com.springboot.HotelBookingSystem.model.Customer;
 import com.springboot.HotelBookingSystem.model.User;
 import com.springboot.HotelBookingSystem.service.CustomerService;
@@ -58,5 +61,17 @@ import com.springboot.HotelBookingSystem.service.UserService;
 			
 			
 			}
+		
+		@GetMapping("/customer/getone/{cid}")
+		public ResponseEntity<?> getOne(@PathVariable("cid") int cid) {
+			try {
+				Customer customer = customerService.getById(cid);
+				return ResponseEntity.ok().body(customer);
+			} catch (InvalidIdException e) {
+				// TODO Auto-generated catch block
+				return ResponseEntity.badRequest().body(e.getMessage());
+			}
+			
+		}
 	}
 
